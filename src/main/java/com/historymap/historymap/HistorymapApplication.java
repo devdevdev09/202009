31c7aac9ec85380540d1ee1c29a1a4a99e634364
dev09 <devdevdevdevdev09@gmail.com>
2020-09-02 17:15:59 +0900

    init repo

---
+++ src/main/java/com/historymap/historymap/HistorymapApplication.java
@@ -0,0 +1,13 @@
+package com.historymap.historymap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistorymapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistorymapApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/historymap/historymap/HistorymapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.historymap.historymap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistorymapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
