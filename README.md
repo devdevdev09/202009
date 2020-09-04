136addce1417ee57696564f629758b6957cf1780
devdevdev09 <devdevdevdevdev09@gmail.com>
2020-09-04 22:11:51 +0900

    add controller

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # 202009
+
+## History Map
+
+## TODO
+- 가볼곳, 간곳 등을 사용자 별로 저장할 수 있는 기능
+- Kakao 지도 API 사용
+
+### 2020-09-04
+- README.md 작성
+- API KEY 재발급
---
+++ src/main/java/com/historymap/historymap/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.historymap.historymap.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+@RequestMapping("/")
+@Controller
+public class MainController {
+
+    @Value("${map.api.key}")
+    String MAP_API_KEY;
+
+    // @RequestMapping("/index")
+    @GetMapping("/index")
+    public String index(Model model){
+        model.addAttribute("MAP_API_KEY", MAP_API_KEY);
+
+        return "index";
+    }   
+}
