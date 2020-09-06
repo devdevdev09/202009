64c064e76ff70a1583a1ed2530886c1640ddd4be
devdevdev09 <devdevdevdevdev09@gmail.com>
2020-09-06 21:10:32 +0900

    remove value annotation

--- src/main/java/com/historymap/historymap/controller/MainController.java
+++ src/main/java/com/historymap/historymap/controller/MainController.java
@@ -2,7 +2,6 @@ package com.historymap.historymap.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
@@ -10,13 +9,8 @@ import org.springframework.ui.Model;
 @Controller
 public class MainController {
 
-    @Value("${map.api.key}")
-    String MAP_API_KEY;
-
     @GetMapping("/index")
     public String index(Model model){
-        model.addAttribute("MAP_API_KEY", MAP_API_KEY);
-
         return "index";
     }   
 }
